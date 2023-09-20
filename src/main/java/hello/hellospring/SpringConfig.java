dedc7a12e7a46e778936808775ba760c33369849
nylonmusk <2912rla@gmail.com>
2023-09-20 18:23:54 +0900

    feat : create TimeTraceAop

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,5 +1,6 @@
 package hello.hellospring;
 
+import hello.hellospring.aop.TimeTraceAop;
 import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.JpaMemberRepository;
 import hello.hellospring.repository.MemberRepository;
@@ -14,6 +15,7 @@ import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
 
+
     private final MemberRepository memberRepository;
 
     @Autowired
@@ -25,6 +27,11 @@ public class SpringConfig {
     public MemberService memberService() {
         return new MemberService(memberRepository);
     }
+
+//    @Bean
+//    public TimeTraceAop timeTraceAop() {
+//        return new TimeTraceAop();
+//    }
 
 //    @Bean
 //    public MemberRepository memberRepository() {
